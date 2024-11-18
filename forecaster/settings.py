6da4d3b78265e44731e37d23428a599228b3bd6b
Entoni Mzilangve <entoni.mzilangve@dualbootpartners.com>
2024-11-18 07:33:02 +0300

    Connected to postgresql

--- forecaster/settings.py
+++ forecaster/settings.py
@@ -53,14 +53,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "forecaster.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv('SQL_ENGINE', "django.db.backends.sqlite3"),
+        "NAME": os.getenv('SQL_DATABASE', BASE_DIR / "db.sqlite3"),
+        "USER": os.getenv('SQL_USER', "user"),
+        "PASSWORD": os.getenv('SQL_PASSWORD', "password"),
+        "HOST": os.getenv('SQL_HOST', "localhost"),
+        "PORT": os.getenv('SQL_PORT', "5432"),
     }
 }
 
