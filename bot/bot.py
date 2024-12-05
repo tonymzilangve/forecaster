aca20e682c2bd7b24f2774cb6ff9c66e847dc446
Entoni Mzilangve <entoni.mzilangve@dualbootpartners.com>
2024-12-05 04:00:24 +0300

    Upgraded bot as dc service

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import telebot
 import requests
 import os
 from telebot.types import Message
+from telebot import types
 from dotenv import load_dotenv
 
 
@@ -10,28 +11,35 @@ load_dotenv()
 bot = telebot.TeleBot(token=os.getenv('BOT_TOKEN'))
 
 
-def extract_arg(arg):
-    return arg.split()[1:]
-
-
-@bot.message_handler(commands=['weather'])
-def start(message: Message):
-    city = extract_arg(message.text)
-    bot.reply_to(message=message, text=city)
-    
-    url = f"http://127.0.0.1:8000/api/v2/weather/{city[0]}"
+@bot.callback_query_handler(func=lambda call: True) 
+def forecaster(call):
+    city = call.data
+    url = f"http://api:8000/api/v2/weather/{city}"
     response = requests.get(url, params={"source_type": "telegram"})
-    
-    weather = response.json()[city[0]]
+
+    weather = response.json()[city]
     answer = (
-        f'Погода в городе {city[0].capitalize()}:\n'
+        f'Погода в городе {city.capitalize()}:\n'
         f'Температура: {weather["temperature"]}\n'
         f'Давление: {weather["pressure"]}\n'
         f'Скорость ветра: {weather["wind_speed"]}\n'
         f'Источник запроса: {weather["source_type"]}\n'
     )
     
-    bot.send_message(chat_id=message.chat.id, text=answer)
+    bot.send_message(call.from_user.id, text=answer)
 
 
-bot.polling()
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, text='Введите ваш город: ')
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_all(message):
+    markup = types.InlineKeyboardMarkup()
+    item = types.InlineKeyboardButton('Узнать погоду', callback_data=message.text)
+    markup.add(item)
+    bot.send_message(message.chat.id, f"{message.text}".capitalize(), reply_markup=markup)
+
+
+bot.polling(none_stop=True)
